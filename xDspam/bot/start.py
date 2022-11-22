6f0793c3e4a9b9230af50958acf2eac71deadab6
Fabin๏ <116137433+FabinoXD@users.noreply.github.com>
2022-11-22 15:19:42 +0530

    Update start.py

--- xDspam/bot/start.py
+++ xDspam/bot/start.py
@@ -3,33 +3,34 @@
 # ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ :- @xDspamBots
 # ɢɪᴛʜᴜʙ :- @FabinoXD ""
 
-import os
-import sys
-import asyncio
-from xDspam import (SUDO_USERS, OWNER_ID, ALIVE_PIC)
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
+from xDspam import ALIVE_PIC
 
 Pic = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/a50a32854921ea49b665f.jpg"
 Hn = "/"
 btn = InlineKeyboardMarkup(
-                [[
-                    InlineKeyboardButton(
-                        "• Channel •", url="https://t.me/EnoughBio"),
-                    InlineKeyboardButton(
-                        "• Support •", url="https://t.me/DevilsHeavenMF")
-                ], [
-                    InlineKeyboardButton(
-                        "• Repo •", url="https://github.com/The-HellBot/ArrayCore")
-                ]]
+    [
+        [
+            InlineKeyboardButton("• Channel •", url="https://t.me/EnoughBio"),
+            InlineKeyboardButton("• Support •", url="https://t.me/DevilsHeavenMF"),
+        ],
+        [
+            InlineKeyboardButton(
+                "• Repo •", url="https://github.com/FabinoXD/xDspam"
             )
- 
-                   
-@Client.on_message(filters.private & filters.incoming & filters.command(['start'], prefixes=Hn))
+        ],
+    ]
+)
+
+
+@Client.on_message(
+    filters.private & filters.incoming & filters.command(["start"], prefixes=Hn)
+)
 async def _start(_, ok: Message):
-    msg = f"**Hello [{ok.from_user.first_name}](tg://user?id={ok.from_user.id}) !** \n\n __ • I'm xDspam An Advance Spambot__ \n\n **Click Below Buttons for More Info**"           
+    msg = f"**Hello [{ok.from_user.first_name}](tg://user?id={ok.from_user.id}) !** \n\n __ • I'm xDspam An Advance Spambot__ \n\n **Click Below Buttons for More Info**"
     if ".jpg" in Pic or ".png" in Pic:
-              await ok.reply_photo(Pic, caption=msg, reply_markup=btn)
+        await ok.reply_photo(Pic, caption=msg, reply_markup=btn)
     if ".mp4" in Pic or ".MP4," in Pic:
-              await ok.reply_video(Pic, caption=msg, reply_markup=btn)
+        await ok.reply_video(Pic, caption=msg, reply_markup=btn)
