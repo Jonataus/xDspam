3d09d0084eea0bf26feff732dead24dce5cee9bb
Fabin๏ <116137433+FabinoXD@users.noreply.github.com>
2022-11-22 15:24:24 +0530

    Update joinleave.py

--- xDspam/module/joinleave.py
+++ xDspam/module/joinleave.py
@@ -3,9 +3,10 @@
 # ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ :- @xDspamBots
 # ɢɪᴛʜᴜʙ :- @FabinoXD ""
 
-from xDspam import (OWNER_ID, HNDLR, SUDO_USERS, LOGS_CHANNEL)
 from pyrogram import Client, filters
 from pyrogram.types import Message
+
+from xDspam import HNDLR, LOGS_CHANNEL, SUDO_USERS
 
 
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["join"], prefixes=HNDLR))
@@ -13,51 +14,64 @@ from pyrogram.types import Message
 async def join(xspam: Client, e: Message):
     chat = e.text[6:]
     if chat.isnumeric():
-        return await e.reply_text("Can't join a chat with chat id. Give username or invite link.")
+        return await e.reply_text(
+            "Can't join a chat with chat id. Give username or invite link."
+        )
     try:
-      await xspam.join_chat(chat)
-      await e.reply_text("**ᴊᴏɪɴ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ✅ **")
+        await xspam.join_chat(chat)
+        await e.reply_text("**ᴊᴏɪɴ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ✅ **")
     except Exception as ex:
         await e.reply_text(f"**ERROR:** \n\n{str(ex)}")
     if LOGS_CHANNEL:
-         try:
-             await xspam.send_message(LOGS_CHANNEL, f"Joined New Chat \n\n Chat: {chat} \n Cmd By User: {e.from_user.id}")
-         except Exception as a:
-             print(a)
-             pass
+        try:
+            await xspam.send_message(
+                LOGS_CHANNEL,
+                f"Joined New Chat \n\n Chat: {chat} \n Cmd By User: {e.from_user.id}",
+            )
+        except Exception as a:
+            print(a)
 
-@Client.on_message(filters.user(SUDO_USERS) & filters.command(["leave"], prefixes=HNDLR))
+
+@Client.on_message(
+    filters.user(SUDO_USERS) & filters.command(["leave"], prefixes=HNDLR)
+)
 @Client.on_message(filters.me & filters.command(["leave"], prefixes=HNDLR))
 async def leave(xspam: Client, e: Message):
     HNY = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
     if len(e.text) > 7:
         chat = HNY[0]
         try:
-           await xspam.leave_chat(chat)
-           await e.reply_text("**Left Successfully ✅ **")
+            await xspam.leave_chat(chat)
+            await e.reply_text("**Left Successfully ✅ **")
         except Exception as ex:
-           await e.reply_text(f"**ERROR:** \n\n{str(ex)}")
+            await e.reply_text(f"**ERROR:** \n\n{str(ex)}")
     else:
         chat = e.chat.id
         ok = e.from_user.id
         if int(chat) == int(ok):
-            return await e.reply_text(f"Usage: {HNDLR}leave <chat username or id> or {HNDLR}leave (type in Group for Direct leave)")
+            return await e.reply_text(
+                f"Usage: {HNDLR}leave <chat username or id> or {HNDLR}leave (type in Group for Direct leave)"
+            )
         if int(chat) == -1001321613309:
-              return e.reply_text("**Error**")
+            return e.reply_text("**Error**")
         try:
-           await xspam.leave_chat(chat)
-           await e.reply_text("**Left Successfully ✅ **")
+            await xspam.leave_chat(chat)
+            await e.reply_text("**Left Successfully ✅ **")
         except Exception as ex:
-           await e.reply_text(f"**ERROR:** \n\n{str(ex)}")
+            await e.reply_text(f"**ERROR:** \n\n{str(ex)}")
         if LOGS_CHANNEL:
-           try:
-                await xspam.send_message(LOGS_CHANNEL, f"Leaved Chat \n\n Chat: {chat} \n Cmd By User: {e.from_user.id}")
-           except Exception as a:
-             print(a)
-             pass
+            try:
+                await xspam.send_message(
+                    LOGS_CHANNEL,
+                    f"Leaved Chat \n\n Chat: {chat} \n Cmd By User: {e.from_user.id}",
+                )
+            except Exception as a:
+                print(a)
 
 
-@Client.on_message(filters.user(SUDO_USERS) & filters.command(["react"], prefixes=HNDLR))
+@Client.on_message(
+    filters.user(SUDO_USERS) & filters.command(["react"], prefixes=HNDLR)
+)
 @Client.on_message(filters.me & filters.command(["react"], prefixes=HNDLR))
 async def react_here(xspam: Client, message: Message):
     try:
@@ -72,10 +86,12 @@ async def react_here(xspam: Client, message: Message):
             message_id=message.reply_to_message.id,
             emoji=reaction,
         )
-    except Exception as e:
+    except Exception:
         await xspam.send_reaction(
             chat_id=message.chat.id,
             message_id=message.reply_to_message.id,
             emoji="❤️",
         )
-        return await message.reply_text("» ɪɴᴠᴀʟɪᴅ ʀᴇᴀᴄᴛɪᴏɴ sᴘᴇᴄɪғɪᴇᴅ, ʀᴇᴀᴄᴛᴇᴅ ᴛᴏ ᴛʜᴇ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ : ❤️")
+        return await message.reply_text(
+            "» ɪɴᴠᴀʟɪᴅ ʀᴇᴀᴄᴛɪᴏɴ sᴘᴇᴄɪғɪᴇᴅ, ʀᴇᴀᴄᴛᴇᴅ ᴛᴏ ᴛʜᴇ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ : ❤️"
+        )
